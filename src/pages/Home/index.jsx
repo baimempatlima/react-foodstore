05d37d60d04935b2702834876847b76461804ddb
Baimempatlima <97828149+baimempatlima@users.noreply.github.com>
2022-04-30 18:31:56 +0700

    Update index.jsx

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,296 +1,208 @@
-import React, { useEffect, useState } from "react";
-
+import React, { useState, useEffect } from "react";
+// import { Button } from "react-bootstrap";
+import { Link, useNavigate } from "react-router-dom";
 import axios from "axios";
+// import { Form } from "react-bootstrap";
+import { Listbox } from "@headlessui/react";
 
-import { Link } from "react-router-dom";
-import { Spinner } from "react-bootstrap";
-import Category from "../../components/Category";
-import Tags from "../../components/Tags";
-import Card from "../../components/Card";
+const defaultAddress = [{ name: "Choose One Address:" }];
 
-export default function Home() {
-  const [products, setProducts] = useState([]);
-  const [categories, setCategories] = useState([]);
-  const [categorySend, setCategorySend] = useState({});
-  const [page, setPage] = useState(1);
-  const [tags, setTags] = useState([]);
-  const [tagSend, setTagSend] = useState([]);
-  const [query, setQuery] = useState("");
-  const [detailUser, setDetailUser] = useState([]);
+export default function Carts() {
+  const navigate = useNavigate();
+  const [user, setUser] = useState([]);
+  const [carts, setCarts] = useState([]);
   const [token, setToken] = useState(localStorage.getItem("token"));
   const headersAuth = { headers: { Authorization: `Bearer ${token}` } };
-  const [carts, setCarts] = useState([]);
-  const [cartCount, setCartCount] = useState(0);
-
-  const getProduct = async () => {
-    try {
-      await axios.get(`http://localhost:3000/api/products`).then((res) => setProducts(res.data.data));
-    } catch (err) {
-      console.log(err);
-    }
-  };
-  const getCagetories = async () => {
-    try {
-      await axios.get(`http://localhost:3000/api/categories`).then((res) => {
-        setCategories(res.data);
-      });
-    } catch (err) {
-      console.log(err);
-    }
-  };
-
-  const getTags = async () => {
-    try {
-      await axios.get(`http://localhost:3000/api/tags`).then((res) => {
-        if (res.data.error !== 1) {
-          setTags(res.data);
-        }
-      });
-    } catch (err) {
-      console.log(err);
-    }
-  };
-
-  const getPage = async () => {
-    try {
-      let apiLink = `http://localhost:3000/api/products?`;
-
-      let categorySelect = "";
-      if (categorySend.name === undefined) {
-        categorySelect = "";
-      } else if (categorySend.name === "Default:") {
-        categorySelect = "";
-      } else {
-        categorySelect = `category=${categorySend.name}`;
-      }
-
-      let tagsSelected = [];
-      if (tagSend.length !== 0) {
-        tagsSelected = tagSend.map((item, index) => `tags[${index}]=${item.name}`).join("&");
-      } else {
-        tagsSelected = "";
-      }
-
-      let querySelected = "";
-      if (query.length !== 0) {
-        querySelected = `q=${query}`;
-      } else {
-        querySelected = "";
-      }
-
-      let skip = "";
-      if (page === 1) {
-        skip = "";
-      } else {
-        skip = `skip=${(page - 1) * 10}`;
-      }
-      let linkApiSend = `${apiLink}${categorySelect !== "" ? `${categorySelect}&&` : ""}${tagsSelected !== "" ? `${tagsSelected}&&` : ""}${querySelected !== "" ? querySelected : ""}${skip !== "" ? skip : ""}`;
-
-      await axios(linkApiSend).then((res) => setProducts(res.data.data));
-    } catch (err) {
-      console.log(err);
-    }
-  };
-
-  const pageNext = () => {
-    setPage(page + 1);
-  };
-
-  const pagePrev = () => {
-    setPage(page - 1);
-  };
+  const [getAddress, setGetAddress] = useState([]);
+  const [addresSelect, setAddresSelect] = useState(defaultAddress[0]);
+  const [validation, setValidation] = useState([]);
 
   const authCheck = async () => {
     try {
       await axios.get(`http://localhost:3000/auth/me`, headersAuth).then((res) => {
         if (res.data.error !== 1) {
-          setDetailUser(res.data);
+          setUser(res.data);
         }
       });
     } catch (error) {
-      console.log(error, "Auth Error");
+      console.log(error, "auth error");
     }
   };
 
-  const logout = async (e) => {
-    e.preventDefault();
+  const getCart = async () => {
     try {
-      await axios.post(`http://localhost:3000/auth/logout`, null, headersAuth).then((res) => {
-        if (res.data.error !== 1) {
-          localStorage.removeItem("token");
-          setDetailUser([]);
-          setToken("");
-        }
-      });
-    } catch (error) {
-      console.log(error, "logut error");
-    }
-  };
-
-  const getCartCount = async () => {
-    try {
-      await axios.get(`http://localhost:3000/api/carts`, headersAuth).then((res) => {
-        setCartCount(res.data.map((item) => item.qty).reduce((preVal, currentVal) => preVal + currentVal, 0));
-        setCarts(res.data);
-      });
+      await axios.get(`http://localhost:3000/api/carts`, headersAuth).then((res) => setCarts(res.data));
     } catch (err) {
-      console.log(err, "cant get the count carts");
+      console.log(err, "data carts not found");
     }
   };
 
-  const cartsAdd = async (product) => {
+  const quantityMin = async (cart) => {
     try {
-      const { category, tags, description, createdAt, updatedAt, __v, _id, ...remainProduct } = product;
-      let newProductData = remainProduct;
-
-      if (carts.find((item) => item.product._id === _id)) {
-        newProductData = carts.map((item) => ({ ...item, qty: item.product._id === _id ? item.qty + 1 : item.qty }));
+      let payload = [];
+      if (carts.find((item, index) => item[index] === cart[index])) {
+        payload = carts.map((item) => ({ ...item, qty: item.product.name === cart.name && item.qty !== 0 ? item.qty - 1 : item.qty }));
       } else {
-        newProductData = [...carts, { qty: 1, product: product, user: detailUser, ...remainProduct }];
+        payload = [...carts];
       }
 
-      let cartsDataCount = newProductData.map((item) => item.qty).reduce((preVal, currentVal) => preVal + currentVal, 0);
-      setCartCount(cartsDataCount);
-      setCarts(newProductData);
-
-      await axios.put(`http://localhost:3000/api/carts`, { items: newProductData }, headersAuth);
+      setCarts(payload);
+      await axios.put(`http://localhost:3000/api/carts`, { items: payload }, headersAuth); // sending the data everytime the button is clicked
     } catch (error) {
-      console.log(error, "cant add carts");
+      console.log(error);
     }
   };
 
+  const quantityPlus = async (cart) => {
+    try {
+      let payload = [];
+
+      if (carts.find((item, index) => item[index] === cart[index])) {
+        payload = carts.map((item) => ({ ...item, qty: item.product.name === cart.name ? item.qty + 1 : item.qty }));
+      } else {
+        payload = [...carts];
+      }
+
+      setCarts(payload);
+      await axios.put(`http://localhost:3000/api/carts`, { items: payload }, headersAuth);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const deleteCart = async (cart) => {
+    try {
+      let payload = [];
+      if (carts.find((item, index) => item[index] === cart[index])) {
+        payload = carts.filter((item) => item.name !== cart.name);
+      } else {
+        payload = [...carts];
+      }
+
+      setCarts(payload);
+      await axios.put(`http://localhost:3000/api/carts/api/carts`, { items: payload }, headersAuth);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const showAddress = async () => {
+    try {
+      await axios.get(`http://localhost:3000/api/delivery-addresses`, headersAuth).then((res) => setGetAddress(res.data.data));
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const checkout = async () => {
+    try {
+      let payload = { user: user, delivery_fee: 0, order_items: carts, delivery_address: addresSelect };
+      if (addresSelect.name !== "Choose One Address:") {
+        await axios.post(`http://localhost:3000/api/orders`, payload, headersAuth).then((res) => {
+          setValidation(res.data);
+          if (res.data.error !== 1) {
+            navigate(`/invoice/user/${res.data._id}`);
+          }
+        });
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  };
   useEffect(() => {
-    getCartCount();
-    getProduct();
-    getCagetories();
-    getTags();
+    getCart();
     authCheck();
+    showAddress();
   }, []);
 
-  useEffect(() => {
-    getPage();
-  }, [categorySend, tagSend, query, page]);
-
   return (
-    <div>
-      <div>
-        <div>
-          <div>
-            <nav className="navbar navbar-expand-lg  shadow-lg p-3 mb-5 bg-body  rounded">
-              <div className="container">
-                <a className="navbar-brand" href="/">
-                  BUY FOOD
-                </a>
-                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="/navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                  <span className="navbar-toggler-icon"></span>
-                </button>
-                <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                  {!!detailUser.full_name ? (
-                    <div className="ms-auto">
-                      <ul className="navbar-nav  mb-2 mb-lg-0 ">
-                        <li className="nav-item">
-                          <p style={{ marginRight: "10px" }} className={`${detailUser.role === "admin" ? "text-uppercase" : ""}"text-capitalize mt-2 badge bg-secondary "`}>{`${detailUser.full_name}`}</p>
-                        </li>
-                        <li className="nav-item ">
-                          <a className="nav-link" href="/cart/user">
-                            Cart <div style={{ fontSize: "10px", display: "inline" }}>{cartCount}</div>
-                          </a>
-                        </li>
-                        <li className="nav-item ">
-                          <Link to="/account/user" className="nav-link active ml-2 " aria-current="page">
-                            Account
-                          </Link>
-                        </li>
-                        <li className="nav-item" onClick={(e) => logout(e)}>
-                          <Link to="/" className="nav-link">
-                            Logout
-                          </Link>
-                        </li>
-                      </ul>
-                    </div>
-                  ) : (
-                    <div className="ms-auto">
-                      <ul className="navbar-nav  mb-2 mb-lg-0 ">
-                        <li className="nav-item ">
-                          <Link to="/login" className="nav-link active" aria-current="page">
-                            Login
-                          </Link>
-                        </li>
-                        <li className="nav-item">
-                          <Link to="/register" className="nav-link">
-                            Register
-                          </Link>
-                        </li>
-                      </ul>
-                    </div>
-                  )}
-                </div>
-              </div>
-            </nav>
+    <div className="container-fluid">
+      <div className="container-fluid mt-5">
+        <div className="container shadow-lg p-3 mb-5 bg-body rounded">
+          <div className="flex-container">
+            <div className="text-start">
+              <Link to="/" className="btn btn-warning">
+                <i className="fa-solid fa-arrow-left"></i>
+              </Link>
+            </div>
           </div>
-        </div>
-      </div>
-      <div>
-        <div>
-          <div className="container">
-            <div className="container">
-              <div className="row justify-content-center">
-                <div className="w-100 flex-container justify-content-center mb-3">
-                  <div style={{ marginRight: "5px" }} className="mt-0">
-                    <input type="text" className="form-control styleborder " placeholder="search your food or drinks" onChange={(e) => setQuery(e.target.value)} />
-                  </div>
-                  <div className="mt-2">
-                    <Category categoryData={categories} setCategorySend={setCategorySend} />
-                  </div>
+          <div className="row justify-content-center mt-4">
+            <h2 style={{ fontSize: "40px", display: "inline" }}>
+              My <i className="fas fa-shopping-cart text-danger"></i>{" "}
+            </h2>
+            <hr />
+            <div className="col-md-5 mt-3">
+              <h1>Menu</h1>
+              {carts.length === 0 ? (
+                <div>
+                  {" "}
+                  <h2 style={{ fontSize: "20px" }}>Food or Drinks Not Found</h2>
                 </div>
-
-                <div className=" w-100 flex-container justify-content-center ">
-                  <div style={{ marginRight: "5px", marginTop: "9px" }}>Tags:</div>
-                  <div>
-                    <Tags tagsData={tags} setTagSend={setTagSend} />
+              ) : (
+                carts.map((cart) => (
+                  <div key={cart._id} className="flex-container">
+                    <div className="text-start m-1 w-100">
+                      <h1>{cart.name}</h1>
+                      <h1>Price : {new Intl.NumberFormat("id-ID", { style: "currency", currency: "IDR" }).format(cart.price * cart.qty)}</h1>
+                      <h1 style={{ display: "inline" }}>Quantity : </h1>
+                      <i onClick={() => quantityPlus(cart)} className="fa fa-plus m-2" aria-hidden="true"></i>
+                      <b>{cart.qty}</b>
+                      <i onClick={() => quantityMin(cart)} className="fa fa-minus m-2" aria-hidden="true"></i>
+                      <hr />
+                    </div>
+                    <div className="w-100 text-end">
+                      <img src={`http://localhost:3000/images/products/${cart.image_url}`} className="mt-2" alt="" style={{ borderRadius: "15px", height: "80px", weight: "80px" }} />
+                    </div>
+                    <div className="m-1 w-50 text-center" onClick={() => deleteCart(cart)}>
+                      <i className="fa fa-trash mt-4" aria-hidden="true"></i>
+                    </div>
                   </div>
-                </div>
+                ))
+              )}
+            </div>
+            <div className="col-md-3 mt-3 text-start borderleft">
+              <h1 style={{ display: "inline", marginRight: "5px" }}>Delivery Address</h1>
+              <i class="fa-solid fa-location-dot"></i>
+              <div className="mt-2">
+                <Listbox value={addresSelect} onChange={setAddresSelect}>
+                  <Listbox.Button className="btn-lg" style={{ fontSize: "14px", display: "inline", width: "100%" }}>
+                    {addresSelect.name}
+                  </Listbox.Button>
+                  <Listbox.Options className=" text-start dropdownadd border-danger">
+                    {getAddress.map((address) => (
+                      <Listbox.Option key={address._id} value={address} className="text-start dropdownopop">
+                        {address.name}
+                        {address.kelurahan}
+                        {address.kabupaten}
+                        {address.provinsi}
+                      </Listbox.Option>
+                    ))}
+                  </Listbox.Options>
+                </Listbox>
+              </div>
+              <div className="text-end mt-2" style={{ fontSize: "15px" }}>
+                <Link to="/form-address/user" className="btn btn-outline-warning btn-sm" alt="notfound">
+                  <i className="fa-solid fa-circle-plus"> </i> Add New Address
+                </Link>
+              </div>
+              <div className="text-end mt-2">
+                {addresSelect.name === "Choose One Address:" ? <div className="valid-feedback text-end">please add a delivery address</div> : ""}
+                {validation.error === 1 ? <p className="mb-2 text-red-400">{validation.message}</p> : ""}
               </div>
             </div>
-            <hr />
-            {products.length !== 0 ? (
-              <div className="row justify-content-center">
-                {products.map((product) => (
-                  <div className="col-md-3 m-3">
-                    <div>
-                      <Card productData={product} userData={detailUser} onClick={() => cartsAdd(product)} />
-                    </div>
-                  </div>
-                ))}
+            <div className="col-md-3 text-end borderleft mt-2">
+              <div className="mt-2">
+                <h1 className="text-start">Total:</h1>
               </div>
-            ) : (
-              <div className="justify-content-center">
-                <Spinner animation="border" />
+              <div className="mt-2">
+                <p style={{ fontSize: "28px" }}> {new Intl.NumberFormat("id-ID", { style: "currency", currency: "IDR" }).format(carts.map((item) => item.qty * item.price).reduce((preVal, currentVal) => preVal + currentVal, 0))}</p>
+                <button className="btn btn-danger" onClick={() => checkout()}>
+                  CHECK OUT
+                </button>
               </div>
-            )}
+            </div>
           </div>
         </div>
-      </div>
-      <div>
-        <nav className="pagination justify-content-center">
-          <div className="page">
-            {page === 1 ? (
-              ""
-            ) : (
-              <a className="previous round" onClick={() => pagePrev()} href="/#">
-                &#8249;
-              </a>
-            )}
-            <div className="py-0.5 px-6 rounded-md shadow-sm items-center justify-center border">{page}</div>
-            {products.data?.length <= 10 && page * 10 < products.count ? (
-              <a className="next round" onClick={() => pageNext()} href="/#">
-                &#8250;
-              </a>
-            ) : (
-              ""
-            )}
-          </div>
-        </nav>
       </div>
     </div>
   );
